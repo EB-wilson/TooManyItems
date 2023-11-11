24bd59f46041bfd0bee33205fdea638f720f689d
EBwilson <2534946881@qq.com>
2023-11-11 12:09:14 +0800

    添加部分注释

---
+++ src/main/java/tmi/recipe/types/RecipeItem.java
@@ -0,0 +1,18 @@
+package tmi.recipe.types;
+
+import arc.graphics.g2d.TextureRegion;
+
+public abstract class RecipeItem<T> {
+  public final T item;
+
+  public RecipeItem(T item) {
+    this.item = item;
+  }
+
+  public abstract int ordinal();
+  public abstract int typeID();
+  public abstract String name();
+  public abstract String localizedName();
+  public abstract TextureRegion icon();
+  public abstract boolean hidden();
+}
